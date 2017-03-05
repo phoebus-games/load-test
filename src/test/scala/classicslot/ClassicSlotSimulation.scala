1c7168ec511f49de113f87f729a7d643b278ab11
Alex Collins <alex.e.c@gmail.com>
2017-03-05 19:16:27 +0000

    homepage

--- src/test/scala/classicslot/ClassicSlotSimulation.scala
+++ src/test/scala/classicslot/ClassicSlotSimulation.scala
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 class ClassicSlotSimulation extends Simulation {
 
   val httpConf = http
-    .baseURL("http://games.alexecollins.com/api/games/classic-slot")
+    .baseURL("http://games.alexecollins.com/")
     .contentTypeHeader("application/json")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
@@ -14,9 +14,10 @@ class ClassicSlotSimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   val scn = scenario("Classic Slot")
-    .exec(http("Get Game").get("").check(status.is(200)))
+    .exec(http("Homepage").get("").check(status.is(200)))
+    .exec(http("Get Game").get("/api/games/classic-slot").check(status.is(200)))
     .pause(7)
-    .exec(http("Spin").post("/spins").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
+    .exec(http("Spin").post("/api/games/classic-slot/spins").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
     .pause(3)
 
   setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
--- src/test/scala/roulette/RouletteSimulation.scala
+++ src/test/scala/roulette/RouletteSimulation.scala
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 class RouletteSimulation extends Simulation {
 
   val httpConf = http
-    .baseURL("http://games.alexecollins.com/api/games/roulette")
+    .baseURL("http://games.alexecollins.com")
     .contentTypeHeader("application/json")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
@@ -14,12 +14,13 @@ class RouletteSimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   val scn = scenario("Classic Slot")
-    .exec(http("Get Game").get("").check(status.is(200)))
+    .exec(http("Homepage").get("").check(status.is(200)))
+    .exec(http("Get Game").get("/api/games/roulette").check(status.is(200)))
     .pause(7)
-    .exec(http("Add Bet").post("/bets/red").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
-    .exec(http("Add Bet").post("/bets/black").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
-    .exec(http("Add Bet").post("/bets/number").body(StringBody("{\"amount\": 1, \"number\": 2}")).check(status.is(201)))
-    .exec(http("Spin").post("/spins").body(StringBody("{}")).check(status.is(201)))
+    .exec(http("Add Bet").post("/api/games/roulette/bets/red").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
+    .exec(http("Add Bet").post("/api/games/roulette/bets/black").body(StringBody("{\"amount\": 1}")).check(status.is(201)))
+    .exec(http("Add Bet").post("/api/games/roulette/bets/number").body(StringBody("{\"amount\": 1, \"number\": 2}")).check(status.is(201)))
+    .exec(http("Spin").post("/api/games/roulette/spins").body(StringBody("{}")).check(status.is(201)))
     .pause(3)
 
   setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
